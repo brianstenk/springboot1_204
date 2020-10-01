6a0cb2d5a4843ff9c13fc36a58d25f582c711bde
Brian Nkurunungi <nkurunungib@gmail.com>
2020-09-30 22:09:21 -0500

    The Form Validations - Easyway

---
+++ src/main/java/com/example/springboot1_204/Springboot1204Application.java
@@ -0,0 +1,13 @@
+package com.example.springboot1_204;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot1204Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot1204Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboot1_204/Springboot1204ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot1_204;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot1204ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
